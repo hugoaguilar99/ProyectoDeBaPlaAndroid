5872b4be1d281e917eecbffe9b7bd1ab6c069b75
Victor <a320663@uach.mx>
2020-11-26 16:27:25 -0700

    json support added

--- app/src/main/java/com/example/proyectdebaplaandroid/MainActivity.kt
+++ app/src/main/java/com/example/proyectdebaplaandroid/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.proyectdebaplaandroid
 
+import android.app.VoiceInteractor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.RequestFuture
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val queue = Volley.newRequestQueue(this)
+        val url  = "https://quacklabs.herokuapp.com/about"
+
+        val jsonRequest : StringRequest = StringRequest(Request.Method.GET, url ,
+            Response.Listener {response->
+                Log.i("JSON", response.toString())
+        }, Response.ErrorListener { error ->
+                Log.e("JSON","Error en la peticion ${error.toString()}")
+        } )
+        queue.add(jsonRequest)
     }
 }
